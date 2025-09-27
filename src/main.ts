c0849ed0b90c896a826ec7b0b61807490b7e70e2
AloysiussG <aloysiusgsetog@gmail.com>
2025-09-27 16:25:03 +0700

    feat: init api call for login from nest backend

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,27 @@ import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { LoggerService } from '@nestjs/common';
 
+// sementara tidak menggunakan .env supaya mudah &
+// dapat langsung dipakai untuk test
+export const URL = {
+  NEXT_FRONTEND_APP_URL: 'http://localhost:3000',
+  NEST_BACKEND_APP_URL: 'http://localhost:8000',
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // winston
   const logger = app.get<LoggerService>(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  // Enable CORS
+  app.enableCors({
+    origin: URL.NEXT_FRONTEND_APP_URL,
+    credentials: false,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  });
 
   await app.listen(process.env.PORT ?? 8000);
 }
